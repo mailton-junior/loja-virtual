a2e9e356b9d14b3ed11215390c848b05dbd1ed02
Mailton Junior <mailtonjr7@gmail.com>
2024-10-28 11:56:10 -0300

    Getter and Setter

--- src/main/java/org/project/loja_virtual/model/PurchaseInvoice.java
+++ src/main/java/org/project/loja_virtual/model/PurchaseInvoice.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 @Table(name = "PURCHASE_INVOICE")
@@ -30,4 +31,107 @@ public class PurchaseInvoice {
 
     @Temporal(TemporalType.DATE)
     private Date purchaseDate;
+
+    @ManyToOne(targetEntity = Person.class)
+    @JoinColumn(name = "PERSON_ID", nullable = false,
+    foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "FK_PURCHASE_INVOICE_PERSON"))
+    private Person person;
+
+    @ManyToOne
+    @JoinColumn(name = "ACCOUNT_PAYABLE_ID", nullable = false,
+    foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "FK_PURCHASE_INVOICE_ACCOUNT_PAYABLE"))
+    private AccountPayable accountPayable;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public String getInvoiceSeries() {
+        return invoiceSeries;
+    }
+
+    public void setInvoiceSeries(String invoiceSeries) {
+        this.invoiceSeries = invoiceSeries;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public BigDecimal getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(BigDecimal totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public BigDecimal getDiscountValue() {
+        return discountValue;
+    }
+
+    public void setDiscountValue(BigDecimal discountValue) {
+        this.discountValue = discountValue;
+    }
+
+    public BigDecimal getICMSValue() {
+        return ICMSValue;
+    }
+
+    public void setICMSValue(BigDecimal ICMSValue) {
+        this.ICMSValue = ICMSValue;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public AccountPayable getAccountPayable() {
+        return accountPayable;
+    }
+
+    public void setAccountPayable(AccountPayable accountPayable) {
+        this.accountPayable = accountPayable;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        PurchaseInvoice that = (PurchaseInvoice) object;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
