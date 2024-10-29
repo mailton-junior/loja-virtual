c6a4a62af82602c1b576e3a63900447bffc37fa7
Mailton Junior <mailtonjr7@gmail.com>
2024-10-29 14:05:31 -0300

    Update LojaVirtualApplication.java

--- src/main/java/org/project/loja_virtual/LojaVirtualApplication.java
+++ src/main/java/org/project/loja_virtual/LojaVirtualApplication.java
@@ -1,9 +1,18 @@
 package org.project.loja_virtual;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.project.*"})
+@EntityScan(basePackages = "org.project.loja_virtual.model")
+@EnableJpaRepositories(basePackages = {"org.project.loja_virtual.repository"})
+@EnableTransactionManagement
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
